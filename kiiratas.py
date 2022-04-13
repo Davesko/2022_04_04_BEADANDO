2f2f194308bbb3f494389284b24fcbbc6e8ce7e4
mzsolti08 <monizsolt03@gmail.com>
2022-04-13 12:22:42 +0200

    Harc fejlesztve, + kiiratas (szint + rework)

--- kiiratas.py
+++ kiiratas.py
@@ -1,20 +1,84 @@
-#saját statok ----
-szint = 0
-Hp = 100
-Mana = 100
-Ero = 1
-Gyorsasag = 1
-penz = 0
-
-#kiiratás függvény ----
+#kiiratás class ----
 class Kiiratas:
+    #saját statok ----
+    exp = 0
+    szint = 0
+    Hp = 100
+    Mana = 100
+    Ero = 1
+    Gyorsasag = 1
+    penz = 0
     def adatok(self):
+        # szint ----
+        if self.exp >= 100 and self.exp <= 219:
+            self.szint = 1
+            self.Hp = 105
+            self.Mana = 101
+            self.Ero = 2
+        elif self.exp >= 220 and self.exp <= 349:
+            self.szint = 2
+            self.Hp = 107
+            self.Mana = 102
+            self.Ero = 3
+        elif self.exp >= 350 and self.exp <= 489:
+            self.szint = 3
+            self.Hp = 110
+            self.Mana = 103
+            self.Ero = 4
+        elif self.exp >= 490 and self.exp <= 599:
+            self.szint = 4
+            self.Hp = 112
+            self.Mana = 105
+            self.Ero = 6
+        elif self.exp >= 600 and self.exp <= 739:
+            self.szint = 5
+            self.Hp = 118
+            self.Mana = 105
+            self.Ero = 7
+        elif self.exp >= 740 and self.exp <= 889:
+            self.szint = 6
+            self.Hp = 120
+            self.Mana = 106
+            self.Ero = 8
+            self.Gyorsasag = 2
+        elif self.exp >= 890 and self.exp <= 999:
+            self.szint = 7
+            self.Hp = 124
+            self.Mana = 107
+            self.Ero = 9
+            self.Gyorsasag = 3
+        elif self.exp >= 1000 and self.exp <= 1209:
+            self.szint = 8
+            self.Hp = 127
+            self.Mana = 108
+            self.Ero = 10
+            self.Gyorsasag = 5
+        elif self.exp >= 1210 and self.exp <= 1499:
+            self.szint = 9
+            self.Hp = 130
+            self.Mana = 109
+            self.Ero = 12
+            self.Gyorsasag = 7
+        elif self.exp >= 1500:
+            self.szint = 10
+            self.Hp = 150
+            self.Mana = 115
+            self.Ero = 15
+            self.Gyorsasag = 10
+        else:
+            self.szint = 0
+            self.Hp = 100
+            self.Mana = 100
+            self.Ero = 1
+            self.Gyorsasag = 1
+
         print(f'\n------------------\n'
               f'Jelenlegi értékeid:\n'
               f'------------------')
-        print(f'Szinted: {szint}')
-        print(f'Életerőd: {Hp}')
-        print(f'Manád: {Mana}')
-        print(f'Erőd: {Ero}')
-        print(f'Gyorsaságod: {Gyorsasag}')
+        print(f'Tapasztalat pontjaid: {self.exp}')
+        print(f'Szinted: {self.szint}')
+        print(f'Életerőd: {self.Hp}')
+        print(f'Manád: {self.Mana}')
+        print(f'Erőd: {self.Ero}')
+        print(f'Gyorsaságod: {self.Gyorsasag}')
         print(f'------------------')
--- main.py
+++ main.py
@@ -12,49 +12,173 @@ print(f'\n------------------------\n'
       f'A HATALMAS HARCOS JÁTÉK\n'
       f'------------------------\n')
 
-#harc ----
-bekert = input("Mit szeretnél csinálni? (alvás, harc, fejlesztés)")
+#profil ----
+reg = input("Regisztráltál már (y/n):")
+while True:
+      if reg == "y":
+            bejelentkezes = input('Felhasználónév megadása bejelentkezéshez (max 15 karakter):')
+            felhaszalok = open('felhasznalok.txt', 'r')
+            adatok = felhaszalok.readlines()
+            for sorok in adatok:
+                  sor = sorok.split(';')
+                  if bejelentkezes == sor[0]:
+                        print(f"Bejelentkeztél: {sor[0]}\n"
+                              f"Tapasztalati pontok: {sor[1]}\n"
+                              f"Szint: {sor[2]}\n"
+                              f"Hp: {sor[3]}\n"
+                              f"Mana: {sor[4]}\n"
+                              f"Ero: {sor[5]}\n"
+                              f"Gyorsaság: {sor[6]}\n"
+                              f"Pénz: {sor[7]}\n")
 
-if bekert == "harc" or bekert == "harcolas" or bekert == "harcolás":
-      if int(kiiratas.szint) >= 0 and int(kiiratas.szint) < 3:
+      elif reg == "n":
+            regisztracio = input('Felhasználónév megadása regisztráláshoz (max 15 karakter):')
+            with open('felhasznalok.txt', 'a') as felhaszalok:
+                  felhaszalok.write(f"{regisztracio};"
+                                    f"{stat.exp};"
+                                    f"{stat.szint};"
+                                    f"{stat.Hp};"
+                                    f"{stat.Mana};"
+                                    f"{stat.Ero};"
+                                    f"{stat.Gyorsasag};"
+                                    f"{stat.penz}\n")
+      elif reg != "y" or reg != "n":
+            print("Ez nem érthető számomra, írj y-t vagy n-t, vagy írd kicsivel!")
 
-            szornyelete = random.randint(20, 30)
-            szornysebzese = random.randint(harc, harc + 5)
-            szornyszint = random.randint(1, 6)
-            szornyerteke = random.randint(szornyszint, szornyszint + 5)
+#műveletek ----
+bekert = input("Mit szeretnél csinálni? [alvás] | [harc] | [fejlesztés] | [stat]::")
+while True:
+      #stat ----
+      if bekert == "stat":
+            stat.adatok()
+            bekert = input("Mit szeretnél csinálni? (alvás, harc, fejlesztés, stat)")
+            print('--------------------------------')
+      #harc ----
+      elif bekert == "harc" or bekert == "harcolas" or bekert == "harcolás":
+            if int(stat.szint) >= 0 and int(stat.szint) < 3:
 
-            print(f'\nA sebzési szinted: {kiiratas.Ero}\n'
-                        f'--------------------------------')
-            print(f'Egy {szornyszint} szintű szöryel harcolsz!')
-            print(f'\n HP :{szornyelete} \n Sebzés: {szornysebzese} \n Értéke: {szornyerteke} érme')
-            print(f'---------------------------------\n')
+                  #szörny statok ----
+                  szornyelete = random.randint(20, 30)
+                  szornysebzese = random.randint(harc, harc + 5)
+                  szornyszint = random.randint(1, 6)
+                  szornyerteke = random.randint(szornyszint, szornyszint + 5)
 
-            while szornyelete >= 0 or kiiratas.Hp >= 0:
-                  valasztas = input("Támadás / Védekezés")
-                  if valasztas == "tamadas" or valasztas == "támadás" or valasztas == "Támadás":
-                        sebzes = kiiratas.Ero + random.randint(1,2)
+                  #támadás ----
+                  print('--------------------------------')
+                  print(f'A sebzési szinted: {stat.Ero}\n'
+                              f'--------------------------------')
+                  print(f'Egy {szornyszint} szintű szöryel harcolsz!')
+                  print(f'\n HP :{szornyelete} \n Sebzés: {szornysebzese} \n Értéke: {szornyerteke} érme')
+                  print(f'---------------------------------\n')
 
-                        randomkrit = random.randint(1,10)
-                        kritikus = random.randint(kiiratas.Ero + 2 , kiiratas.Ero + 8)
-                        if randomkrit >= 9:
-                              szornyelete = szornyelete - kritikus
-                              print(f'A szönyet kritikusan eltaláltad, élete {kritikus} HP-val csökkent. A szörnynek {szornyelete} HP-ja maradt.')
+                  while szornyelete >= 0 or stat.Hp >= 0:
+                        valasztas = input("Támadás / Védekezés")
+                        if valasztas == "tamadas" or valasztas == "támadás" or valasztas == "Támadás" or valasztas == "t":
+                              sebzes = stat.Ero + random.randint(1,2)
+
+                              randomkrit = random.randint(1,10)
+                              kritikus = random.randint(stat.Ero + 2 , stat.Ero + 8)
+                              if randomkrit >= 9:
+                                    szornyelete = szornyelete - kritikus
+                                    if szornyelete <= 0:
+                                          print('A szörnynek nem maradt élete!')
+                                    else:
+                                          print(f'A szönyet kritikusan eltaláltad, élete {kritikus} HP-val csökkent. A szörnynek {szornyelete} HP-ja maradt.')
+                              else:
+                                    szornyelete = szornyelete - sebzes
+                                    if szornyelete <= 0:
+                                          print('A szörnynek nem maradt élete!')
+                                    else:
+                                          print(f'A szöny élete csökkent {sebzes} HP-val. A szörnynek {szornyelete} HP-ja maradt.')
                         else:
-                              szornyelete = szornyelete - sebzes
-                              print(f'A szöny élete csökkent {sebzes} HP-val. A szörnynek {szornyelete} HP-ja maradt.')
+                            print('Nem tudom hogy ez mit jelent!')
 
 
-                  if szornyelete == 0 or szornyelete < 0:
-                        print(f'Gratulálok, legyőzted az ellenfelet, nehéz csata volt! \t A jutalmad {szornyerteke}')
-                        kiiratas.penz += szornyerteke
-                        break
+                        if szornyelete <= 0:
+                              print(f'Gratulálok, legyőzted az ellenfelet, nehéz csata volt! \t A jutalmad {szornyerteke * 5} tapasztalat pont')
+                              stat.penz += szornyerteke * 2
+                              stat.exp += szornyerteke * 5
+                              stat.adatok()
+                              break
+            if int(stat.szint) > 5 and int(stat.szint) < 10:
+                  harc = random.randint(3, 9)
+                  #szörny statok ----
+                  szornyelete = random.randint(50, 75)
+                  szornysebzese = random.randint(harc, harc + 13)
+                  szornyszint = random.randint(5, 9)
+                  szornyerteke = random.randint(szornyszint, szornyszint + 12)
 
-      if kiiratas.szint > 5 and kiiratas.szint < 10:
-            harc = random.randint(4, 14)
+                  #támadás ----
+                  print('--------------------------------')
+                  print(f'A sebzési szinted: {stat.Ero}\n'
+                              f'--------------------------------')
+                  print(f'Egy {szornyszint} szintű szöryel harcolsz!')
+                  print(f'\n HP :{szornyelete} \n Sebzés: {szornysebzese} \n Értéke: {szornyerteke} érme')
+                  print(f'---------------------------------\n')
 
-      if kiiratas.szint > 10 and kiiratas.szint < 20:
-            harc = random.randint(9, 25)
+                  while szornyelete >= 0 or stat.Hp >= 0:
+                        valasztas = input("Támadás / Védekezés")
+                        if valasztas == "tamadas" or valasztas == "támadás" or valasztas == "Támadás":
+                              sebzes = stat.Ero + random.randint(1,2)
 
-      else:
-            bekert = input("Mit szeretnél csinálni? (alvás, harc, vásárlás)")
+                              randomkrit = random.randint(1,10)
+                              kritikus = random.randint(stat.Ero + 2 , stat.Ero + 8)
+                              if randomkrit >= 9:
+                                    szornyelete = szornyelete - kritikus
+                                    print(f'A szönyet kritikusan eltaláltad, élete {kritikus} HP-val csökkent. A szörnynek {szornyelete} HP-ja maradt.')
+                              else:
+                                    szornyelete = szornyelete - sebzes
+                                    print(f'A szöny élete csökkent {sebzes} HP-val. A szörnynek {szornyelete} HP-ja maradt.')
+                        else:
+                            print('Nem tudom hogy ez mit jelent!')
+
+
+                        if szornyelete == 0 or szornyelete < 0:
+                              print(f'Gratulálok, legyőzted az ellenfelet, nehéz csata volt! \t A jutalmad {szornyerteke}')
+                              stat.penz += szornyerteke
+                              break
+            if int(stat.szint) > 10:
+                  harc = random.randint(9, 25)
+                  #szörny statok ----
+                  szornyelete = random.randint(100, 160)
+                  szornysebzese = random.randint(harc, harc + 17)
+                  szornyszint = random.randint(10, 16)
+                  szornyerteke = random.randint(szornyszint, szornyszint + 18)
+
+                  #támadás ----
+                  print('--------------------------------')
+                  print(f'A sebzési szinted: {stat.Ero}\n'
+                              f'--------------------------------')
+                  print(f'Egy {szornyszint} szintű szöryel harcolsz!')
+                  print(f'\n HP :{szornyelete} \n Sebzés: {szornysebzese} \n Értéke: {szornyerteke} érme')
+                  print(f'---------------------------------\n')
+
+                  while szornyelete >= 0 or stat.Hp >= 0:
+                        valasztas = input("Támadás / Védekezés")
+                        if valasztas == "tamadas" or valasztas == "támadás" or valasztas == "Támadás":
+                              sebzes = stat.Ero + random.randint(1,2)
+
+                              randomkrit = random.randint(1,10)
+                              kritikus = random.randint(stat.Ero + 2 , stat.Ero + 8)
+                              if randomkrit >= 9:
+                                    szornyelete = szornyelete - kritikus
+                                    print(f'A szönyet kritikusan eltaláltad, élete {kritikus} HP-val csökkent. A szörnynek {szornyelete} HP-ja maradt.')
+                              else:
+                                    szornyelete = szornyelete - sebzes
+                                    print(f'A szöny élete csökkent {sebzes} HP-val. A szörnynek {szornyelete} HP-ja maradt.')
+                        else:
+                            print('Nem tudom hogy ez mit jelent!')
+
+
+                        if szornyelete == 0 or szornyelete < 0:
+                              print(f'Gratulálok, legyőzted az ellenfelet, nehéz csata volt! \t A jutalmad {szornyerteke}')
+                              stat.penz += szornyerteke
+                              break
+            else:
+                  bekert = input("Mit szeretnél csinálni? (alvás, harc, vásárlás, stat)")
+      #hiba ----
+      elif bekert != "stat" and bekert != "harc" and bekert != "harcolas" and bekert != "harcolás":
+            print('Nem tudom, hogy ez mit jelent!')
+            bekert = input("Mit szeretnél csinálni? (alvás, harc, fejlesztés, stat)")
+
 #harc vége ----
